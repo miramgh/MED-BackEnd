a511c2fff4617c8b7664c4dc279e0ad144c9b8d0
Mira M-Gh <55577054+miramgh@users.noreply.github.com>
2021-08-28 13:28:34 +0200

    Update app.js

--- app.js
+++ app.js
@@ -69,7 +69,7 @@ app.use((req, res, next) => {
 });
 
 // 3) ROUTES
-//app.use('/' , (req, res)=>{res.send("hello")})
+
 app.use('/api/cases', caseRouter);
 app.use('/api/users', userRouter);
 app.use('/api/recentQSession' , QSessionRouter)
